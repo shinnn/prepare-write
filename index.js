2055cc603006bfaeaf9785f0f507fc82f2392ad4
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-05-31 10:46:56 +0900

    use const, arrow function, template literal and object shorthand
    [BREAKING] drop support fro Node.js < 4.0.0

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 'use strict';
 
-var inspect = require('util').inspect;
-var pathLib = require('path');
+const inspect = require('util').inspect;
+const pathLib = require('path');
 
-var dirname = pathLib.dirname;
-var resolvePath = pathLib.resolve;
+const dirname = pathLib.dirname;
+const resolvePath = pathLib.resolve;
 
-var fs = require('graceful-fs');
-var isDir = require('is-dir');
-var mkdirp = require('mkdirp');
+const fs = require('graceful-fs');
+const isDir = require('is-dir');
+const mkdirp = require('mkdirp');
 
-var PATH_ERROR = 'Expected a file path (string)';
+const PATH_ERROR = 'Expected a file path (string)';
 
 module.exports = function prepareWrite(filePath) {
   if (typeof filePath !== 'string') {
-    return Promise.reject(new TypeError(PATH_ERROR + ', but got ' + inspect(filePath) + '.'));
+    return Promise.reject(new TypeError(`${PATH_ERROR}, but got ${inspect(filePath)}.`));
   }
 
   if (filePath.length === 0) {
-    return Promise.reject(new Error(PATH_ERROR + ', but got \'\' (empty string).'));
+    return Promise.reject(new Error(`${PATH_ERROR}, but got '' (empty string).`));
   }
 
-  var absoluteFilePath = resolvePath(filePath);
+  const absoluteFilePath = resolvePath(filePath);
 
   return Promise.all([
-    new Promise(function(resolve, reject) {
-      mkdirp(dirname(absoluteFilePath), {fs: fs}, function(err, firstDir) {
+    new Promise((resolve, reject) => {
+      mkdirp(dirname(absoluteFilePath), {fs}, (err, firstDir) => {
         if (err) {
           reject(err);
           return;
@@ -34,19 +34,17 @@ module.exports = function prepareWrite(filePath) {
         resolve(firstDir);
       });
     }),
-    new Promise(function(resolve, reject) {
-      isDir(filePath, function(err, yes) {
+    new Promise((resolve, reject) => {
+      isDir(filePath, (err, yes) => {
         if (err) {
           resolve();
           return;
         }
 
         if (yes) {
-          var error = new Error(
-            'Tried to create a file as ' +
-            absoluteFilePath +
-            ', but a directory with the same name already exists.'
-          );
+          const error = new Error(`Tried to create a file as ${
+            absoluteFilePath
+          }, but a directory with the same name already exists.`);
           error.code = 'EISDIR';
           error.path = absoluteFilePath;
           error.syscall = 'open';
@@ -58,7 +56,5 @@ module.exports = function prepareWrite(filePath) {
         resolve();
       });
     })
-  ]).then(function(results) {
-    return results[0];
-  });
+  ]).then(result => result[0]);
 };
