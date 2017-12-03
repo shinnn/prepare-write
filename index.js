e2369393eccb3df38be85c4216758464a055ba32
Shinnosuke Watanabe <snnskwtnb@gmail.com>
2017-12-03 10:40:57 +0900

    update dependencies and devDependencies
    Highlight: spaces -> tabs https://github.com/shinnn/eslint-config/commit/e92384d999f10fd0e477e6ada6b979c48977a2f7

--- index.js
+++ index.js
@@ -10,58 +10,58 @@ const mkdirp = require('mkdirp');
 const PATH_ERROR = 'Expected a file path (string)';
 
 module.exports = function prepareWrite(...args) {
-  const argLen = args.length;
+	const argLen = args.length;
 
-  if (argLen !== 1) {
-    return Promise.reject(new RangeError(`Expected 1 argument (string), but got ${
-      argLen === 0 ? 'no' : argLen
-    } arguments instead.`));
-  }
+	if (argLen !== 1) {
+		return Promise.reject(new RangeError(`Expected 1 argument (string), but got ${
+			argLen === 0 ? 'no' : argLen
+		} arguments instead.`));
+	}
 
-  const [filePath] = args;
+	const [filePath] = args;
 
-  if (typeof filePath !== 'string') {
-    return Promise.reject(new TypeError(`${PATH_ERROR}, but got ${inspectWithKind(filePath)}.`));
-  }
+	if (typeof filePath !== 'string') {
+		return Promise.reject(new TypeError(`${PATH_ERROR}, but got ${inspectWithKind(filePath)}.`));
+	}
 
-  if (filePath.length === 0) {
-    return Promise.reject(new Error(`${PATH_ERROR}, but got '' (empty string).`));
-  }
+	if (filePath.length === 0) {
+		return Promise.reject(new Error(`${PATH_ERROR}, but got '' (empty string).`));
+	}
 
-  const absoluteFilePath = resolvePath(filePath);
+	const absoluteFilePath = resolvePath(filePath);
 
-  return Promise.all([
-    new Promise((resolve, reject) => {
-      mkdirp(dirname(absoluteFilePath), {fs}, (err, firstDir) => {
-        if (err) {
-          reject(err);
-          return;
-        }
+	return Promise.all([
+		new Promise((resolve, reject) => {
+			mkdirp(dirname(absoluteFilePath), {fs}, (err, firstDir) => {
+				if (err) {
+					reject(err);
+					return;
+				}
 
-        resolve(firstDir);
-      });
-    }),
-    new Promise((resolve, reject) => {
-      isDir(filePath, (err, yes) => {
-        if (err) {
-          resolve();
-          return;
-        }
+				resolve(firstDir);
+			});
+		}),
+		new Promise((resolve, reject) => {
+			isDir(filePath, (err, yes) => {
+				if (err) {
+					resolve();
+					return;
+				}
 
-        if (yes) {
-          const error = new Error(`Tried to create a file as ${
-            absoluteFilePath
-          }, but a directory with the same name already exists.`);
-          error.code = 'EISDIR';
-          error.path = absoluteFilePath;
-          error.syscall = 'open';
-          reject(error);
+				if (yes) {
+					const error = new Error(`Tried to create a file as ${
+						absoluteFilePath
+					}, but a directory with the same name already exists.`);
+					error.code = 'EISDIR';
+					error.path = absoluteFilePath;
+					error.syscall = 'open';
+					reject(error);
 
-          return;
-        }
+					return;
+				}
 
-        resolve();
-      });
-    })
-  ]).then(result => result[0]);
+				resolve();
+			});
+		})
+	]).then(result => result[0]);
 };
--- test.js
+++ test.js
@@ -1,86 +1,89 @@
 'use strict';
 
+const {lstat} = require('fs');
+const {promisify} = require('util');
 const {resolve} = require('path');
 
-const lstat = require('lstat');
 const prepareWrite = require('.');
 const rmfr = require('rmfr');
 const test = require('tape');
 
+const promisifiedLstat = promisify(lstat);
+
 test('prepareWrite()', async t => {
-  t.plan(13);
+	t.plan(13);
 
-  await rmfr('tmp');
+	await rmfr('tmp');
 
-  prepareWrite('tmp/0/1').then(async dir => {
-    t.ok(
-      (await lstat('tmp/0')).isDirectory(),
-      'should create a parent directory of the file.'
-    );
+	prepareWrite('tmp/0/1').then(async dir => {
+		t.ok(
+			(await promisifiedLstat('tmp/0')).isDirectory(),
+			'should create a parent directory of the file.'
+		);
 
-    t.ok(
-      (await lstat('tmp')).isDirectory(),
-      'should create ancestor directories of the file.'
-    );
+		t.ok(
+			(await promisifiedLstat('tmp')).isDirectory(),
+			'should create ancestor directories of the file.'
+		);
 
-    t.equal(
-      dir,
-      resolve('tmp'),
-      'should be resolved with the path of the first created directory.'
-    );
-  }).catch(t.fail);
+		t.equal(
+			dir,
+			resolve('tmp'),
+			'should be resolved with the path of the first created directory.'
+		);
+	}).catch(t.fail);
 
-  prepareWrite(__filename).then(dir => {
-    t.equal(dir, null, 'should be resolved with null when it creates no directories.');
-  }).catch(t.fail);
+	prepareWrite(__filename).then(dir => {
+		t.equal(dir, null, 'should be resolved with null when it creates no directories.');
+	}).catch(t.fail);
 
-  prepareWrite(resolve('index.js', 'foo')).catch(({code}) => {
-    t.equal(code, 'EEXIST', 'should fail when it cannot create directories.');
-  }).catch(t.fail);
+	prepareWrite(resolve('index.js', 'foo')).catch(({code}) => {
+		t.equal(code, 'EEXIST', 'should fail when it cannot create directories.');
+	}).catch(t.fail);
 
-  prepareWrite(__dirname).catch(err => {
-    t.equal(
-      err.toString(),
-      `Error: Tried to create a file as ${
-        __dirname
-      }, but a directory with the same name already exists.`,
-      'should fail when a diretory already exists in the target path.'
-    );
+	prepareWrite(__dirname).catch(err => {
+		t.equal(
+			err.toString(),
+			`Error: Tried to create a file as ${
+				__dirname
+			}, but a directory with the same name already exists.`,
+			'should fail when a diretory already exists in the target path.'
+		);
 
-    t.equal(err.code, 'EISDIR', 'should add an appropriate `code` property to the error.');
-    t.equal(err.path, __dirname, 'should add an appropriate `path` property to the error.');
-    t.equal(err.syscall, 'open', 'should add an appropriate `syscall` property to the error.');
-  }).catch(t.fail);
+		t.equal(err.code, 'EISDIR', 'should add an appropriate `code` property to the error.');
+		t.equal(err.path, __dirname, 'should add an appropriate `path` property to the error.');
+		t.equal(err.syscall, 'open', 'should add an appropriate `syscall` property to the error.');
+	}).catch(t.fail);
 
-  prepareWrite(/^/).catch(err => {
-    t.equal(
-      err.toString(),
-      'TypeError: Expected a file path (string), but got /^/ (regexp).',
-      'should fail when it takes a non-string argument.'
-    );
-  }).catch(t.fail);
+	prepareWrite(/^/).catch(err => {
+		t.equal(
+			err.toString(),
+			'TypeError: Expected a file path (string), but got /^/ (regexp).',
+			'should fail when it takes a non-string argument.'
+		);
+	}).catch(t.fail);
 
-  prepareWrite('').catch(err => {
-    t.equal(
-      err.toString(),
-      'Error: Expected a file path (string), but got \'\' (empty string).',
-      'should fail when it takes an empty string.'
-    );
-  }).catch(t.fail);
+	prepareWrite('').catch(err => {
+		t.equal(
+			err.toString(),
+			'Error: Expected a file path (string), but got \'\' (empty string).',
+			'should fail when it takes an empty string.'
+		);
+	}).catch(t.fail);
 
-  prepareWrite().catch(err => {
-    t.equal(
-      err.toString(),
-      'RangeError: Expected 1 argument (string), but got no arguments instead.',
-      'should fail when it takes no arguments.'
-    );
-  }).catch(t.fail);
+	prepareWrite().catch(err => {
+		t.equal(
+			err.toString(),
+			'RangeError: Expected 1 argument (string), but got no arguments instead.',
+			'should fail when it takes no arguments.'
+		);
+	}).catch(t.fail);
 
-  prepareWrite('a', 'b').catch(err => {
-    t.equal(
-      err.toString(),
-      'RangeError: Expected 1 argument (string), but got 2 arguments instead.',
-      'should fail when it takes too many arguments.'
-    );
-  }).catch(t.fail);
+	prepareWrite('a', 'b').catch(err => {
+		t.equal(
+			err.toString(),
+			'RangeError: Expected 1 argument (string), but got 2 arguments instead.',
+			'should fail when it takes too many arguments.'
+		);
+	}).catch(t.fail);
 });
